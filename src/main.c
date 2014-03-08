ea69e8a745fe7533db52ada266bdf99af76fde97
Muesli_Reep <project_bravo@hotmail.com>
2014-03-08 12:41:04 +0100

    Work on main.c
    Commit to sync workplaces

--- src/main.c
+++ src/main.c
@@ -13,6 +13,200 @@
 #include <openssl/crypto.h>
 #include <openssl/ssl.h>
 
+/*
+ *----------------------------------------------------------------
+ *
+ * bitc_load_config --
+ *
+ *----------------------------------------------------------------
+ */
+
+static int
+bitc_load_config(struct config **config,
+                 const char     *configPath)
+{
+   char *defaultPath = NULL;
+   const char *path;
+   int res;
+
+   if (configPath == NULL) {
+      char *dir = bitc_get_directory();
+      defaultPath = safe_asprintf("%s/main.cfg", dir);
+      free(dir);
+      path = defaultPath;
+   } else {
+      path = configPath;
+   }
+   res = config_load(path, config);
+   if (res) {
+      Warning("Please create a minimal config: %s\n", path);
+   }
+   free(defaultPath);
+   return res;
+}
+
+/*
+ *------------------------------------------------------------------------
+ *
+ * bitc_check_config --
+ *
+ *------------------------------------------------------------------------
+ */
+
+static int
+bitc_check_config(void)
+{
+   char *cfgPath;
+   char *ctcPath;
+   char *txPath;
+   char *dir;
+   int res = 0;
+
+   dir = bitc_get_directory();
+   cfgPath = safe_asprintf("%s/main.cfg",      dir);
+   ctcPath = safe_asprintf("%s/contacts.cfg",  dir);
+   txPath  = safe_asprintf("%s/tx-labels.cfg", dir);
+
+   if (!file_exists(dir) || !file_exists(cfgPath)) {
+      printf("\nIt looks like you're a new user. Welcome!\n"
+             "\n"
+             "Note that bitc uses the directory: ~/.bitc to store:\n"
+             " - block headers:        ~/.bitc/headers.dat     -- ~ 20 MB\n"
+             " - peer IP addresses:    ~/.bitc/peers.dat       --  ~ 2 MB\n"
+             " - transaction database: ~/.bitc/txdb            --  < 1 MB\n"
+             " - wallet keys:          ~/.bitc/wallet.cfg      --  < 1 KB\n"
+             " - main config file:     ~/.bitc/main.cfg        --  < 1 KB\n"
+             " - a contacts file:      ~/.bitc/contacts.cfg    --  < 1 KB\n"
+             " - a tx-label file:      ~/.bitc/tx-labels.cfg   --  < 1 KB\n\n");
+   }
+
+   if (!file_exists(dir)) {
+      Log(LGPFX" creating directory: %s\n", dir);
+      res = file_mkdir(dir);
+      if (res) {
+         printf("Failed to create directory '%s': %s\n",
+                dir, strerror(res));
+         goto exit;
+      }
+      res = file_chmod(dir, 0700);
+      if (res) {
+         printf("Failed to chmod 0600 directory '%s': %s\n",
+                dir, strerror(res));
+         goto exit;
+      }
+   }
+   bitc_check_create_file(cfgPath, "config");
+   bitc_check_create_file(txPath, "tx-labels");
+
+   if (!file_exists(ctcPath)) {
+      struct config *cfg;
+
+      bitc_check_create_file(ctcPath, "contacts");
+
+      cfg = config_create();
+      config_setstring(cfg, "1PBP4S44b1ro3kD6LQhBYnsF3fAp1HYPf2", "contact0.addr");
+      config_setstring(cfg, "Support bitc development -- https://bit-c.github.com",
+                       "contact0.label");
+
+      config_setstring(cfg, "1PC9aZC4hNX2rmmrt7uHTfYAS3hRbph4UN", "contact1.addr");
+      config_setstring(cfg, "Free Software Foundation -- https://fsf.org/donate/",
+                       "contact1.label");
+      config_setstring(cfg, "1BTCorgHwCg6u2YSAWKgS17qUad6kHmtQW", "contact2.addr");
+      config_setstring(cfg, "Bitcoin Foundation -- https://bitcoinfoundation.org/donate",
+                       "contact2.label");
+
+      config_setstring(cfg, "3", "contacts.numEntries");
+
+      res = config_write(cfg, ctcPath);
+      if (res) {
+         printf("Failed to save contacts file: %s\n", strerror(res));
+      }
+   }
+
+exit:
+   free(txPath);
+   free(cfgPath);
+   free(ctcPath);
+   free(dir);
+
+   return res;
+}
+
+/*
+ *----------------------------------------------------------------
+ *
+ * bitc_load_misc_config --
+ *
+ *----------------------------------------------------------------
+ */
+
+static void
+bitc_load_misc_config(void)
+{
+   char *defaultPath;
+   char *dir;
+   char *path;
+   int res;
+
+   btc->resolve_peers = config_getbool(btc->config, 1, "resolve.peers");
+
+   dir = bitc_get_directory();
+
+   /*
+    * contacts.
+    */
+   defaultPath = safe_asprintf("%s/contacts.cfg", dir);
+   path = config_getstring(btc->config, defaultPath, "contacts.filename");
+   res = config_load(path, &btc->contactsCfg);
+   if (res) {
+      Warning("Please create a minimal config: %s\n", path);
+   }
+   free(defaultPath);
+   free(path);
+
+   /*
+    * tx-label.
+    */
+   defaultPath = safe_asprintf("%s/tx-labels.cfg", dir);
+   path = config_getstring(btc->config, defaultPath, "tx-labels.filename");
+   res = config_load(path, &btc->txLabelsCfg);
+   if (res) {
+      Warning("Please create a minimal config: %s\n", path);
+   }
+   free(defaultPath);
+   free(path);
+
+   free(dir);
+}
+
+/*
+ *---------------------------------------------------------------------
+ *
+ * bitc_openssl_init --
+ *
+ *---------------------------------------------------------------------
+ */
+
+static void
+bitc_openssl_init(void)
+{
+   const char *sslVersion = SSLeay_version(SSLEAY_VERSION);
+   int i;
+
+   Log(LGPFX" using %s -- %u locks\n", sslVersion, CRYPTO_num_locks());
+
+   SSL_library_init();
+   ssl_mutex_array = OPENSSL_malloc(CRYPTO_num_locks() *
+                                    sizeof *ssl_mutex_array);
+   ASSERT(ssl_mutex_array);
+
+   for (i = 0; i < CRYPTO_num_locks(); i++ ){
+      pthread_mutex_init(&ssl_mutex_array[i], NULL);
+   }
+   CRYPTO_set_id_callback(bitc_openssl_thread_id_fun);
+   CRYPTO_set_locking_callback(bitc_openssl_lock_fun);
+}
+
 
 /*
  *---------------------------------------------------------------------
@@ -47,9 +241,20 @@ bitc_usage(void)
 
 int main(int argc, char *argv[])
 {
-	char *errStr = NULL;
-	char *configPath = NULL;
-	int maxPeers = 5;
+	struct secure_area *passphrase	= NULL;
+	const int minPeersInit 		= 50;
+	char *addr_label 			= NULL;
+	char *errStr 				= NULL;
+	char *configPath 			= NULL;
+	char *testStr 				= NULL;
+	int maxPeers 				= 5;
+	bool updateAndExit 			= 0;
+	bool zap 					= 0;
+	bool withui 				= 1;
+	bool encrypt 				= 0;
+	bool getpassword 			= 0;
+	int res 					= 0;
+	int c;
 
 	static const struct option long_opts [] = {
 		{ "config",       required_argument,  0,  'c' },
@@ -61,7 +266,6 @@ int main(int argc, char *argv[])
 		{ "version",      no_argument,        0,  'v' },
 		{  NULL,          0,                  0,   0  },
 	};
-
 
 	while ((c = getopt_long(argc, argv, "a:c:dehn:pt:Tuvz",
 	                           long_opts, NULL)) != EOF) {
@@ -79,7 +283,6 @@ int main(int argc, char *argv[])
 	      }
 	}
 
-
 	if (btc->testnet) {
 		printf("Using testnet.\n");
 		usleep(500 * 1000);
@@ -96,4 +299,25 @@ int main(int argc, char *argv[])
 	  free(logFile);
 	  free(login);
 	}
+
+	util_bumpcoresize();
+	bitc_check_config();
+
+	res = bitc_load_config(&btc->config, configPath);
+	if (res != 0) {
+	  return res;
+	}
+
+	bitc_load_misc_config();
+	btc->lock = mutex_alloc();
+	btc->pw = poolworker_create(10);
+	ipinfo_init();
+	bitc_openssl_init();
+
+	res = bitc_init(passphrase, updateAndExit, maxPeers, minPeersInit, &errStr);
+	if (res) {
+	  goto exit;
+	}
+
+	bitc_daemon(updateAndExit, maxPeers);
 }
